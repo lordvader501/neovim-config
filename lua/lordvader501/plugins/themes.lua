bb1fa53e5ee042e7066bab34c4a92ee602927b43
lordvader501 <60027612+lordvader501@users.noreply.github.com>
2024-07-01 04:17:51 +0530

    added git signs lualine autotag and many more

--- lua/lordvader501/plugins/themes.lua
+++ lua/lordvader501/plugins/themes.lua
@@ -1,9 +1,9 @@
-return { 
-    'rose-pine/neovim', 
+return {
+    'rose-pine/neovim',
     name = 'rose-pine',
-	config = function()
+    config = function()
         require("rose-pine").setup({
-            variant = "auto", -- auto, main, moon, or dawn
+            variant = "auto",      -- auto, main, moon, or dawn
             dark_variant = "main", -- main, moon, or dawn
             dim_inactive_windows = false,
             extend_background_behind_borders = true,
@@ -11,7 +11,7 @@ return {
             enable = {
                 terminal = true,
                 legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-                migrations = true, -- Handle deprecated options automatically
+                migrations = true,        -- Handle deprecated options automatically
             },
 
             styles = {
@@ -68,6 +68,6 @@ return {
                 -- end
             end,
         })
-	vim.cmd("colorscheme rose-pine")
+        vim.cmd("colorscheme rose-pine")
     end
 }
